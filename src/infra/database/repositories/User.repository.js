e14a6c23401976efe3959d32ce4509e3662e0e6b
marcusviini <marcustere28@gmail.com>
2022-04-30 12:35:08 -0300

    feat: add user repository

---
+++ src/infra/database/repositories/User.repository.js
@@ -0,0 +1,33 @@
+import User from '../schemas/User'
+import { CryptoAdapter } from '../../adapter/cripto.adapter'
+
+const cryptoAdapter = new CryptoAdapter()
+
+export class UserRepository {
+    async create(data) {
+        data.password = await cryptoAdapter.createHashPassword(data.password)
+        return await User.create(data)
+    }
+
+    async update(_id, data) {
+        if (data.password) {
+            data.password = await cryptoAdapter.createHashPassword(
+                data.password
+            )
+        }
+
+        return await User.findByIdAndUpdate({ _id }, data)
+    }
+
+    async findUser(data) {
+        return await User.findOne(data)
+    }
+
+    async findUserByCpf(data) {
+        return await User.findOne(data)
+    }
+
+    async findAll() {
+        return await User.find()
+    }
+}
---
+++ src/infra/database/schemas/User.js
@@ -0,0 +1,35 @@
+import mongoose from 'mongoose';
+
+const User = new mongoose.Schema(
+    {
+        nome: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        avatar_name: {
+            type: String,
+            required: false,
+        },
+        avatar_path: {
+            type: String,
+            required: false,
+        },
+        password_reset_token: {
+            type: String,
+            required: false,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+export default new mongoose.model('User', User);
